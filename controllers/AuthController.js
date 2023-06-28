2962acfd3b1e1d004cfb83b7bd41518a9b2dba46
safarilewis <lleewwii1@gmail.com>
2023-06-29 00:18:45 +0300

    file update

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,20 +1,52 @@
 import { v4 as uuidv4 } from 'uuid';
-/* eslint-disable-next-line */
+import sha1 from 'sha1';
+// eslint-disable-next-line
 import redisClient from '../utils/redis';
+import userUtils from '../middleware/user';
 
-export default class AuthController {
-  static async getConnect(req, res) {
-    const { user } = req;
+class AuthController {
+  static async getConnect(request, response) {
+    const Authorization = request.header('Authorization') || '';
+
+    const credentials = Authorization.split(' ')[1];
+
+    if (!credentials) { return response.status(401).send({ error: 'Unauthorized' }); }
+
+    const decodedCredentials = Buffer.from(credentials, 'base64').toString(
+      'utf-8',
+    );
+
+    const [email, password] = decodedCredentials.split(':');
+
+    if (!email || !password) { return response.status(401).send({ error: 'Unauthorized' }); }
+
+    const sha1Password = sha1(password);
+
+    const user = await userUtils.getUser({
+      email,
+      password: sha1Password,
+    });
+
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
     const token = uuidv4();
+    const key = `auth_${token}`;
+    const hoursForExpiration = 24;
 
-    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-    res.status(200).json({ token });
+    await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
+
+    return response.status(200).send({ token });
   }
 
-  static async getDisconnect(req, res) {
-    const token = req.headers['x-token'];
+  static async getDisconnect(request, response) {
+    const { userId, key } = await userUtils.getUserIdAndKey(request);
 
-    await redisClient.del(`auth_${token}`);
-    res.status(204).send();
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+
+    await redisClient.del(key);
+
+    return response.status(204).send();
   }
 }
+
+export default AuthController;
