9c5bace12339d0aa3021063286fa7592f55fc413
safarilewis <lleewwii1@gmail.com>
2023-06-28 17:48:43 +0300

    setting up redis client

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (error) => {
+      console.error('Redis client failed to connect:', error.message || error.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+export const redisClient = new RedisClient();
+export default redisClient;
